dea7431e0f4a538e648da7d04a0f9d991dea629c
Namitha Vijayakumar <namithav@namithas-mbp.lan>
2021-01-02 10:53:58 -0800

    removing unwanted file

--- get_char_position.py
+++
@@ -1,7 +0,0 @@
-import re
-doc = nlp("Is there a Servicenow incident for Mail")
-list_of_strings = ["Servicenow", "incident", "Mail"]
-for s in list_of_strings:
-    res = re.search(s,doc.text)
-    if res:
-        print(s , res.start(), res.end())
