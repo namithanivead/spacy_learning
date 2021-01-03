9f4e35be450bfb857ce2b9d7d5e2ccb243ecb9e8
Namitha Vijayakumar <namithav@unknown.yahoo.com>
2021-01-02 23:06:39 -0800

    adding snow fn

--- chat.py
+++ chat.py
@@ -19,27 +19,35 @@ def who_is(session, query):
                 pass
     return "I don't know about "+query
 
+nlp_ner = spacy.load('/Users/namithav/demo_chatbot/ner')
+nlp_en = spacy.load('en_core_web_sm')
+assert nlp_en.vocab != nlp_ner.vocab
+assert nlp_en.vocab.vectors.to_bytes() == nlp_ner.vocab.vectors.to_bytes()
+ner_new = EntityRecognizer(nlp_en.vocab)
+ner_new.from_bytes(nlp_ner.get_pipe("ner").to_bytes(exclude=["vocab"]))
+nlp_en.add_pipe(ner_new, name="ner_new")
+assert nlp_en.vocab == ner_new.vocab
+
 @register_call("process_text")
 def process_text(session, query):
-    nlp_ner = spacy.load('/Users/namithav/demo_chatbot/ner')
-    nlp_en = spacy.load('en_core_web_sm')
-    assert nlp_en.vocab != nlp_ner.vocab
-    assert nlp_en.vocab.vectors.to_bytes() == nlp_ner.vocab.vectors.to_bytes()
-    ner_new = EntityRecognizer(nlp_en.vocab)
-    ner_new.from_bytes(nlp_ner.get_pipe("ner").to_bytes(exclude=["vocab"]))
-    nlp_en.add_pipe(ner_new, name="ner_new")
-    assert nlp_en.vocab == ner_new.vocab
     try:
         doc = nlp_en(query)
         for ent in doc.ents:
-            return ent.label_
+            if ent.label_ == "SERVICENOW":
+                service_now(query)
+        #return query
     except Exception:
-        for new_query in wikipedia.search(query):
-            try:
-                return wikipedia.summary(new_query)
-            except Exception:
-                pass
+        return False
     return "I don't know about "+query
+
+def service_now(text):
+    print("inside snow fn")
+    #return text
+    doc = nlp_en(text)
+    for ent in doc.ents:
+        print(ent.text, ent.label_)
+    return text
+
 
 
 first_question = "Hi, how are you?"
